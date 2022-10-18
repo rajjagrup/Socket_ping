e8bbcf91197feb1e5310f2e105601374637ddebd
Raj Jagrup <rajjagrup1@gmail.com>
2022-10-18 13:56:18 -0400

    Add a another website to ping delete a print statement

--- Ping/ping_website.py
+++ Ping/ping_website.py
@@ -2,19 +2,21 @@
 #This program will ping websites to see if they are down or up.
 #This will be using sockets
 
-#Import the the libray that is needed for the ping.
+#Import the the libray that is needed for the ping and for sockets.
 import subprocess
 import socket as s 
 
 host = "www.google.com"  #The website trying to ping 
-ip = s.gethostbyname(host)
-
-
+host_1 = "www.strose.edu"
+ip = s.gethostbyname(host) #Stores the host "google.com" to the ip variable.
+ip_1 = s.gethostbyname(host_1)#Stores the 
 ping = subprocess.Popen( #Popen takes in 4 arguments as a function
     ["ping", "-w", "4", host], # The arguments will exute the ping command to the console
     stdout = subprocess.PIPE, # Will write the arguments to the console
     stderr = subprocess.PIPE  
 )
 out, error = ping.communicate()
-print('The IP addrees of ' + host + ' is: ' + ip)
-print(out)
+out1 , error1 = ping.communicate()
+print('The IP addrees of ' + host + ' is: ' + ip) #Will display the ip address of google.com
+print('The IP addrees of ' + host_1 + ' is: ' + ip_1) #Will display the ip address of google.com
+#print(out)
